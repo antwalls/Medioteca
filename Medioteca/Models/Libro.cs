5dc3ea4770270a58898ef995ae63ac9558ac6429
Antonio Paredes <antwalls92@gmail.com>
2015-10-03 18:00:20 +0200

    priemeras pruebas con .NET creacion del primer modelo y primera migracion

---
+++ Medioteca/Models/Libro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Medioteca.Models
+{
+    public class Libro
+    {
+        
+        public int          LibroId          { get; set; }
+        public string       ISBM        { get; set; }
+        public string       Autor       { get; set; }
+        public string       Titulo      { get; set; }
+        public DateTime     F_publi     { get; set; }
+        public string       N_edicion   { get; set; }
+        public string       N_paginas   { get; set; }
+        public string       Argumento   { get; set; }
+
+    }
+}
---
+++ Medioteca/Models/LibroContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Medioteca.Models
+{
+    public class LibroContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public LibroContext() : base("name=LibroContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Medioteca.Models.Libro> Libroes { get; set; }
+    }
+}
